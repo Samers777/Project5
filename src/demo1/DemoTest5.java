8214d7f480801fa16571baea874b40c12ace23c4
Argue <Argue@LAPTOP-O82B69D3>
2025-01-29 19:58:40 -0600

    implemented add method

--- src/demo1/DemoTest5.java
+++ src/demo1/DemoTest5.java
@@ -3,5 +3,16 @@ package demo1;
 public class DemoTest5 {
 	public static void main(String[] args) {
 		System.out.println("Java");
+		
+		int a= 10;
+		int b= 20;
+		
+		int result = a + b;
+			
+		System.out.println(a + b); 
+	}
+	
+	public void add(int a, int b) {
+		System.out.println(a + b);
 	}
 }
